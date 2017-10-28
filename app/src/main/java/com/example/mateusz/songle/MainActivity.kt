9bddd4f358fd17aaa31b188b609d60d9ad3f3638
Lammatian <mateusz.parafinski@gmail.com>
2017-10-28 13:42:28 +0100

    More "fun" with animations, this time something that may actually be useful in the final version of the app; added rectangle shape

--- app/src/main/java/com/example/mateusz/songle/MainActivity.kt
+++ app/src/main/java/com/example/mateusz/songle/MainActivity.kt
@@ -1,23 +1,23 @@
 package com.example.mateusz.songle
 
-import android.animation.ObjectAnimator
+import android.animation.TimeInterpolator
 import android.animation.ValueAnimator
 import android.content.Intent
-import android.graphics.Color
 import android.graphics.Path
 import android.graphics.Typeface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.renderscript.Sampler
 import android.view.View
-import android.view.ViewAnimationUtils
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.BounceInterpolator
 import android.view.animation.LinearInterpolator
-import android.widget.TextView
-import android.widget.ViewAnimator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var count = 0
+    var stretched = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,11 +43,23 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun btnShowHelp(view: View){
-        val path : Path = Path()
-        path.addCircle(txvTest.x-100f, txvTest.y, 100f, Path.Direction.CW)
+    fun btnShowHelp(view: View) {
+        val path = Path()
+        //path.addCircle(txvTest.x-100f, txvTest.y, 100f, Path.Direction.CW)
+        path.moveTo(20f, 20f)
         //var animator = ObjectAnimator.ofFloat(txvTest, "x", txvTest.x, txvTest.x+100f).start()
-        var animator2 = ObjectAnimator.ofFloat(txvTest, TextView.X, TextView.Y, path).start()
+        //var animator2 = ObjectAnimator.ofFloat(txvTest, TextView.X, TextView.Y, path).start()
+        //ObjectAnimator.ofFloat(fab, FloatingActionButton.X, FloatingActionButton.Y, path).start()
+        var animator = ValueAnimator.ofInt(btnStats.width, btnStats.width + 150)
+        animator.addUpdateListener {
+            val value = animator.animatedValue as Int
+            var lp = btnStats.layoutParams
+            lp.width = value
+            btnStats.layoutParams = lp
+        }
+        animator.duration = 1000
+        animator.interpolator = AccelerateInterpolator()
+        animator.start()
 //        animator.addUpdateListener{
 //            val value = it.animatedValue as Float
 //            txvTest.translationY = value
@@ -55,5 +67,19 @@ class MainActivity : AppCompatActivity() {
 //            it.duration = 300
 //        }
 //        animator.start()
+    }
+
+    fun fabChangeView(view: View){
+        var animator : ValueAnimator = if (!stretched) ValueAnimator.ofInt(txvStretch.width, txvStretch.width + 500) else ValueAnimator.ofInt(txvStretch.width, txvStretch.width - 500)
+        animator.addUpdateListener {
+            val value = animator.animatedValue as Int
+            var lp = txvStretch.layoutParams
+            lp.width = value
+            txvStretch.layoutParams = lp
+        }
+        animator.duration = 300
+        animator.interpolator = AccelerateInterpolator()
+        animator.start()
+        stretched = !stretched
     }
 }
