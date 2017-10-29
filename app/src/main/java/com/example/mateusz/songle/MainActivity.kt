d3925270272fca26f7f1a755d01fcc3a6d1eb221
Lammatian <mateusz.parafinski@gmail.com>
2017-10-29 22:40:15 +0000

    Added full animation of menu unscrolling and hiding

--- app/src/main/java/com/example/mateusz/songle/MainActivity.kt
+++ app/src/main/java/com/example/mateusz/songle/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.mateusz.songle
 
-import android.animation.AnimatorSet
-import android.animation.TimeInterpolator
-import android.animation.ValueAnimator
+import android.animation.*
 import android.content.Intent
 import android.graphics.Path
 import android.graphics.Typeface
@@ -13,6 +11,7 @@ import android.renderscript.Sampler
 import android.view.View
 import android.view.animation.AccelerateInterpolator
 import android.view.animation.BounceInterpolator
+import android.view.animation.DecelerateInterpolator
 import android.view.animation.LinearInterpolator
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -20,65 +19,33 @@ class MainActivity : AppCompatActivity() {
 
     var count = 0
     var stretched = false
+    var menuWrapContent = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Change fonts in views
         val balooFont = Typeface.createFromAsset(assets, "fonts/Baloo.ttf")
         val plumpFont = Typeface.createFromAsset(assets, "fonts/Plump.ttf")
         var fontChangeCrawler = FontChangeCrawler(balooFont)
         fontChangeCrawler.replaceFonts(findViewById(android.R.id.content))
 
+        // Get WRAP_CONTENT value for the menu
+        menuWrapContent = menuButtons.layoutParams.width
+
         this.title = "Test"
-        testText.typeface = plumpFont
-    }
-
-    fun btnStartGame(view: View){
-        btnStart.text = "Changed text!"
-        btnStart.elevation = 100F
-        count += 1
-        txvCount.text = "Clicks: " + String.format("%d", count)
-    }
-
-    fun btnShowStats(view: View){
-        val intent = Intent(this, MapsActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun btnShowHelp(view: View) {
-        val path = Path()
-        //path.addCircle(txvTest.x-100f, txvTest.y, 100f, Path.Direction.CW)
-        path.moveTo(20f, 20f)
-        //var animator = ObjectAnimator.ofFloat(txvTest, "x", txvTest.x, txvTest.x+100f).start()
-        //var animator2 = ObjectAnimator.ofFloat(txvTest, TextView.X, TextView.Y, path).start()
-        //ObjectAnimator.ofFloat(fab, FloatingActionButton.X, FloatingActionButton.Y, path).start()
-        var animator = ValueAnimator.ofInt(btnStats.width, btnStats.width + 150)
-        animator.addUpdateListener {
-            val value = animator.animatedValue as Int
-            var lp = btnStats.layoutParams
-            lp.width = value
-            btnStats.layoutParams = lp
-        }
-        animator.duration = 1000
-        animator.interpolator = AccelerateInterpolator()
-        animator.start()
-//        animator.addUpdateListener{
-//            val value = it.animatedValue as Float
-//            txvTest.translationY = value
-//            it.interpolator = LinearInterpolator()
-//            it.duration = 300
-//        }
-//        animator.start()
+        //txvTest.typeface = plumpFont
     }
 
     fun fabChangeView(view: View){
-        btnSecond.alpha = 0f
-        btnSecond.visibility = View.VISIBLE
+        btnStats.alpha = 0f
+        btnStats.visibility = View.VISIBLE
 
-        var animator = ValueAnimator.ofFloat(btnSecond.alpha, 0f, 1f)
+        var animator = ValueAnimator.ofFloat(btnStats.alpha, 0f, 1f)
         animator.addUpdateListener{
             val value = animator.animatedValue as Float
-            btnSecond.alpha = value
+            btnStats.alpha = value
         }
         animator.duration = 300
         animator.interpolator = LinearInterpolator()
@@ -86,17 +53,157 @@ class MainActivity : AppCompatActivity() {
         stretched = !stretched
     }
 
-    fun btnUnstretch(view: View){
-        var animator = ValueAnimator.ofInt(menuButtons.layoutParams.width, (if (stretched) 200 else 700))
-        animator.addUpdateListener {
-            val value = animator.animatedValue as Int
+    // Show or hide top menu along with its buttons
+    fun btnMenu(view: View) {
+        if (!stretched) {
+            showMenu()
+        }
+        else {
+            hideMenu()
+        }
+
+        stretched = !stretched
+    }
+
+    fun showMenu() {
+        var animatorSet = AnimatorSet()
+
+        // Stretch menu
+        // TODO: Adjust starting and ending width better
+        var animatorStretchMenu = ValueAnimator.ofInt(btnStretch.layoutParams.width + 60, 800)
+        animatorStretchMenu.addUpdateListener {
+            val value = animatorStretchMenu.animatedValue as Int
+            var lp = menuButtons.layoutParams
+            lp.width = value
+            menuButtons.layoutParams = lp
+            menuButtons.requestLayout()
+        }
+        animatorStretchMenu.duration = 300
+        animatorStretchMenu.interpolator = AccelerateInterpolator()
+
+        // Animations to show buttons
+        // First, set visibility to visible and opacity to 0
+        btnStats.alpha = 0f
+        btnHelp.alpha = 0f
+        btnRestart.alpha = 0f
+        btnStats.visibility = View.VISIBLE
+        btnHelp.visibility = View.VISIBLE
+        btnRestart.visibility = View.VISIBLE
+
+        // Stats button
+        var showStats = ValueAnimator.ofFloat(btnStats.alpha, 0f, 1f)
+        showStats.addUpdateListener{
+            val value = showStats.animatedValue as Float
+            btnStats.alpha = value
+        }
+        showStats.duration = 300
+        showStats.interpolator = AccelerateInterpolator()
+        showStats.startDelay = 50
+
+        // Help button
+        var showHelp = ValueAnimator.ofFloat(btnHelp.alpha, 0f, 1f)
+        showHelp.addUpdateListener{
+            val value = showHelp.animatedValue as Float
+            btnHelp.alpha = value
+        }
+        showHelp.duration = 300
+        showHelp.interpolator = AccelerateInterpolator()
+        showHelp.startDelay = 100
+
+        // Restart button
+        var showRestart = ValueAnimator.ofFloat(btnRestart.alpha, 0f, 1f)
+        showRestart.addUpdateListener{
+            val value = showRestart.animatedValue as Float
+            btnRestart.alpha = value
+        }
+        showRestart.duration = 300
+        showRestart.interpolator = AccelerateInterpolator()
+        showRestart.startDelay = 150
+
+        // Play all animations together (TODO: Add offset)
+        animatorSet.playTogether(animatorStretchMenu, showStats, showHelp, showRestart)
+        animatorSet.start()
+    }
+
+    fun hideMenu() {
+        var animatorSet = AnimatorSet()
+
+        // Animation to shrink menu
+        // TODO: Adjust starting width better
+        var shrinkMenu = ValueAnimator.ofInt(menuButtons.layoutParams.width, btnStretch.layoutParams.width + 60)
+        shrinkMenu.addUpdateListener {
+            val value = shrinkMenu.animatedValue as Int
             var lp = menuButtons.layoutParams
             lp.width = value
             menuButtons.layoutParams = lp
         }
-        animator.duration = 300
-        animator.interpolator = AccelerateInterpolator()
-        animator.start()
-        stretched = !stretched
+        shrinkMenu.duration = 300
+        shrinkMenu.startDelay = 100
+        shrinkMenu.interpolator = AccelerateInterpolator()
+
+        // Animations to hide buttons
+        btnStats.alpha = 1f
+        btnHelp.alpha = 1f
+        btnRestart.alpha = 1f
+
+        // Hide help button
+        var hideRestart = ValueAnimator.ofFloat(btnRestart.alpha, 1f, 0f)
+        hideRestart.addUpdateListener {
+            val value = hideRestart.animatedValue as Float
+            btnRestart.alpha = value
+        }
+        hideRestart.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationCancel(animation: Animator) {}
+            override fun onAnimationRepeat(animation: Animator) {}
+            override fun onAnimationStart(animation: Animator) {}
+
+            override fun onAnimationEnd(animation: Animator) {
+                btnRestart.visibility = View.GONE
+            }
+        })
+        hideRestart.duration = 300
+        hideRestart.interpolator = DecelerateInterpolator()
+
+        // Hide help button
+        var hideHelp = ValueAnimator.ofFloat(btnHelp.alpha, 1f, 0f)
+        hideHelp.addUpdateListener {
+            val value = hideHelp.animatedValue as Float
+            btnHelp.alpha = value
+        }
+        hideHelp.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationCancel(animation: Animator) {}
+            override fun onAnimationRepeat(animation: Animator) {}
+            override fun onAnimationStart(animation: Animator) {}
+
+            override fun onAnimationEnd(animation: Animator) {
+                btnHelp.visibility = View.GONE
+            }
+        })
+        hideHelp.duration = 300
+        hideHelp.startDelay = 75
+        hideHelp.interpolator = DecelerateInterpolator()
+
+        // Hide stats button
+        var hideStats = ValueAnimator.ofFloat(btnStats.alpha, 1f, 0f)
+        hideStats.addUpdateListener {
+            val value = hideStats.animatedValue as Float
+            btnStats.alpha = value
+        }
+        hideStats.addListener(object : Animator.AnimatorListener {
+            override fun onAnimationCancel(animation: Animator) {}
+            override fun onAnimationRepeat(animation: Animator) {}
+            override fun onAnimationStart(animation: Animator) {}
+
+            override fun onAnimationEnd(animation: Animator) {
+                btnStats.visibility = View.GONE
+            }
+        })
+        hideStats.duration = 300
+        hideStats.startDelay = 150
+        hideStats.interpolator = DecelerateInterpolator()
+
+        // Play animations together
+        animatorSet.playTogether(shrinkMenu, hideStats, hideHelp, hideRestart)
+        animatorSet.start()
     }
 }
